024262c2d5f632d050be3fe13f1e4fe1ea35c826
b-van-b <115096508+b-van-b@users.noreply.github.com>
2023-06-17 23:47:26 +0800

    Add sound effects

--- main.gd
+++ main.gd
@@ -14,12 +14,15 @@ func _ready():
 
 
 func game_over():
+	$Music.stop()
+	$DeathSound.play()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
 
 
 func new_game():
+	$Music.play()
 	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$HUD.update_score(score)
