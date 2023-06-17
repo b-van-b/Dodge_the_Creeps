22d2f1f2bd9b760ef0eca794bfcae4ffae9f0729
b-van-b <115096508+b-van-b@users.noreply.github.com>
2023-06-17 23:38:54 +0800

    Add HUD

--- main.gd
+++ main.gd
@@ -16,10 +16,13 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
 	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 
@@ -31,6 +34,7 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 	
 
 func _on_mob_timer_timeout():
